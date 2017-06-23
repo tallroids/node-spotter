8af7527e28a3795f906f50de5f492a26f12df833
tallroids <har05054@byui.edu>
2017-06-23 15:31:10 -0600

    updated docs and added model functions

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+/*eslint-env node*/
+
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -18,7 +20,9 @@ app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -26,14 +30,14 @@ app.use('/', index);
 app.use('/api', api);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
--- model/public.js
+++ model/public.js
@@ -10,77 +10,185 @@ var config = {
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 }
+var pool = new Pool(config);
 
 function getLocations(callback) {
-  var pool = new Pool(config);
-  pool.connect(function (e) {
+  pool.connect(function () {
     pool.query('SELECT * FROM locations WHERE isPublic=true', function (err, data) {
       if (err) {
         throw err;
       }
-      locations = data.rows;
+      var locations = data.rows;
       callback(null, locations);
     })
   })
 }
 
 function getLocationById(id, callback) {
-  var pool = new Pool(config);
-  pool.connect(function (e) {
+  pool.connect(function () {
     pool.query('SELECT * FROM locations WHERE id=' + id, function (err, data) {
       if (err) {
         throw err;
       }
-      location = data.rows[0];
-      callback(null, location);
+      var result = data.rows[0];
+      callback(null, result);
     })
   })
 }
 
 function getLocationsByCategoryId(id, callback) {
-  var pool = new Pool(config);
-  pool.connect(function (e) {
+  pool.connect(function () {
     pool.query('SELECT l.* FROM locations_categories lc JOIN locations l ON lc.locationId = l.id WHERE lc.categoryId=' + id + ';', function (err, data) {
       if (err) {
         throw err;
       }
-      location = data.rows;
-      callback(null, location);
+      var result = data.rows;
+      callback(null, result);
     });
   })
 }
 
 function createLocation(title, description, lat, lon, isPublic, authorId, callback) {
-  var pool = new Pool(config);
-  pool.connect(function (e) {
+  pool.connect(function () {
     var query = "INSERT INTO locations(title, description, lat, lon, isPublic, authorId) values ('" + title + "', '" + description + "', " + lat + ", " + lon + ", " + isPublic + ", " + authorId + ") RETURNING id;"
     pool.query(query, function (err, data) {
       if (err) {
         throw err;
       }
-      location = data.rows[0];
-      callback(null, location);
+      var result = data.rows[0];
+      callback(null, result);
     })
   })
 }
 
-function updateLocation(id, name, description, isPublic, callback){
-  var pool = new Pool(config);
-  pool.connect(function (e) {
+function updateLocation(id, title, description, isPublic, callback) {
+  pool.connect(function () {
     var query = "UPDATE locations SET (title, description, isPublic) values ('" + title + "', '" + description + "', " + isPublic + ") WHERE id = " + id + "RETURNING id;"
     pool.query(query, function (err, data) {
       if (err) {
         throw err;
       }
-      location = data.rows[0];
-      callback(null, location);
+      id = data.rows[0];
+      callback(null, id);
+    })
+  })
+}
+
+function deleteLocation(id, callback) {
+  pool.connect(function () {
+    var query = "DELETE FROM locations WHERE id = " + id + ";"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err;
+      }
+      var id = data.rows[0];
+      callback(null, id);
+    })
+  })
+}
+
+function getCategories(callback) {
+  pool.connect(function () {
+    var query = "SELECT * FROM categories;"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err
+      }
+      var result = data.rows
+      callback(null, result)
+    })
+  })
+}
+
+function createCategory(title, callback) {
+  pool.connect(function () {
+    var query = "INSERT INTO categories(title) values (" + title + ") RETURNING id;"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err
+      }
+      var result = data.rows[0]
+      callback(null, result)
+    })
+  })
+}
+
+function removeCategory(id, callback) {
+  pool.connect(function () {
+    var query = "DELETE FROM categories WHERE id = " + id + ");"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err
+      }
+      var result = data.rows[0]
+      callback(null, result)
+    })
+  })
+}
+
+function getFavoritesForUser(id, callback) {
+  pool.connect(function () {
+    var query = "SELECT l.* FROM favorites f JOIN locations l ON f.locationId = l.id WHERE f.userId = " + id + ");"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err
+      }
+      var result = data.rows
+      callback(null, result)
+    })
+  })
+}
+
+function removeFavorite(id, callback) {
+  pool.connect(function () {
+    var query = "DELETE FROM favorites WHERE id = " + id + ");"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err
+      }
+      var result = data.rows[0]
+      callback(null, result)
+    })
+  })
+}
+
+function addFavorite(userId, locationId, callback) {
+  pool.connect(function () {
+    var query = "INSERT INTO favorites (userId, locationId) VALUES (" + userId + ", " + locationId + ") RETURNING id;"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err
+      }
+      var result = data.rows[0]
+      callback(null, result)
+    })
+  })
+}
+
+function addLocationCategory(locationId, categoryId, callback) {
+  pool.connect(function () {
+    var query = "INSERT INTO locations_categories (locationId, categoryId) VALUES (" + locationId + ", " + categoryId + ") RETURNING id;"
+    pool.query(query, function (err, data) {
+      if (err) {
+        throw err
+      }
+      var result = data.rows[0]
+      callback(null, result)
     })
   })
 }
 
 module.exports = {
+  getLocations: getLocations,
   getLocationById: getLocationById,
   getLocationsByCategoryId: getLocationsByCategoryId,
   createLocation: createLocation,
-  updateLocation: updateLocation
+  updateLocation: updateLocation,
+  deleteLocation: deleteLocation,
+  getCategories: getCategories,
+  removeCategory: removeCategory,
+  getFavoritesForUser: getFavoritesForUser,
+  removeFavorite: removeFavorite,
+  addFavorite: addFavorite,
+  addLocationCategory: addLocationCategory
 }
