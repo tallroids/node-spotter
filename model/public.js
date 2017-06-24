afefa16c6fb75b94a692a95b2407fc8e76d2110a
Ben Harker <tallroids@gmai.com>
2017-06-23 20:48:05 -0600

    added pg-pool

--- model/public.js
+++ model/public.js
@@ -4,7 +4,7 @@ var config = {
   database: 'spotter', //env var: PGDATABASE
   password: 'spotter_client', //env var: PGPASSWORD
   host: 'localhost', // Server hosting the postgres database
-  port: process.env.PORT || 5432, //env var: PGPORT
+  port: 5432, //env var: PGPORT
   max: 10, // max number of clients in the pool
   idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
 }
