7bedee7d987128d7dccbb1008d99592593c2788f
tallroids <har05054@byui.edu>
2017-06-26 13:03:17 -0600

    worked on home page, implemented materialize

--- routes/index.js
+++ routes/index.js
@@ -2,22 +2,30 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+  res.render('index', {
+    title: 'Spotter'
+  });
 });
-router.get('/getUserById/:id', function(req, res, next){
+router.get('/getUserById/:id', function (req, res, next) {
   var id = req.params.id;
   var user = model.getUserById(id, userOps);
-  function userOps(err, data){
-    res.json({'test':data});
+
+  function userOps(err, data) {
+    res.json({
+      'test': data
+    });
   }
 });
-router.post('/login', function(req, res, next){
+router.post('/login', function (req, res, next) {
   var username = req.post.username;
   var id = req.params.id;
   var user = model.login(username, password, userOps);
-  function userOps(err, data){
-    res.json({'test':data});
+
+  function userOps(err, data) {
+    res.json({
+      'test': data
+    });
   }
 });
 module.exports = router;
