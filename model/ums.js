04795291e79e07821f384a99d52930e82a9184db
Ben Harker <tallroids@gmai.com>
2017-07-01 14:57:03 -0600

    login working

--- model/ums.js
+++ model/ums.js
@@ -1,15 +1,19 @@
-const {
-  Pool
-} = require('pg');
-var config = {
-  user: 'spotter_client', //env var: PGUSER
-  database: 'spotter', //env var: PGDATABASE
-  password: 'spotter_client', //env var: PGPASSWORD
-  host: 'localhost', // Server hosting the postgres database
-  port: 5432, //env var: PGPORT
-  max: 10, // max number of clients in the pool
-  idleTimeoutMillis: 30000, // how long a client is allowed to remain idle before being closed
+const Pool = require('pg-pool');
+const url = require('url');
+const params = url.parse(process.env.DATABASE_URL || 'postgres://spotter_client:spotter_client@localhost:5432/spotter');
+const auth = params.auth.split(':');
+const config = {
+  user: auth[0],
+  password: auth[1],
+  host: params.hostname,
+  port: params.port,
+  database: params.pathname.split('/')[1],
+  ssl: true
 }
+if(params.pathname.split('/')[1] == 'spotter'){
+  config.ssl = false;
+}
+const pool = new Pool(config);
 
 function getUserById(id, callback) {
   var pool = new Pool(config);
@@ -26,21 +30,29 @@ function getUserById(id, callback) {
 
 function login(username, password, callback) {
   var pool = new Pool(config);
+  console.log(pool)
+  var user = {};
   pool.connect(function (e) {
-    pool.query('SELECT * FROM person WHERE id=' + id, function (err, data) {
+    pool.query("SELECT * FROM users WHERE username = '" + username + "';", function (err, data) {
+      console.log(data)
       if (err) {
         throw err;
       }
-      person = data.rows[0];
-      callback(null, person);
+      user = data.rows[0];
+      console.log("user", data)
+      if(user.password == password){
+        callback(null, data)
+      } else {
+      callback("Invalid Login", null);
+      }
     })
   })
 }
 
-function registerUser(fname, lname, email, pass, callback) {
+function registerUser(fname, lname, username, password, callback) {
   var pool = new Pool(config);
   pool.connect(function (e) {
-    pool.query("INSERT INTO users (fname, lname, email, pass) VALUES ('" + fname + "' ,'" + lname + "' ,'" + email + "' ," + pass + " RETURNING id, fname;", function (err, data) {
+    pool.query("INSERT INTO users (fname, lname, username, password) VALUES ('" + fname + "' ,'" + lname + "' ,'" + username + "' ,'" + password + "') RETURNING id, fname;", function (err, data) {
       if (err) {
         throw err;
       }
--- routes/index.js
+++ routes/index.js
@@ -4,22 +4,47 @@ var ums = require('../model/ums');
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('index');
+  res.render('index', {user: "Test", loggedIn: false});
 });
 
 router.get('/register', function (req, res, next) {
-  res.render('register');
+  res.render('register', {
+        loggedIn: false});
 });
 
-router.post('registerUser', function (req, res, next) {
+router.get('/login', function (req, res, next) {
+  res.render('login', {
+        loggedIn: false,
+        message: null
+  });
+});
+
+router.post('/login', function(req, res, next){
+  var username = req.body.username
+  var password = req.body.password
+  var user = ums.login(username, password, function(err, user){
+    console.log("err: ", err, "User: ", user)
+    res.render('index', {
+      loggedIn: true,
+      user: user
+    });
+  });
+});
+
+/* else {
+      res.render('register', {message: "Invalid login, please try again."})
+    }*/
+
+router.post('/registerUser', function (req, res, next) {
   var fname = req.body.fname
   var lname = req.body.lname
   var email = req.body.email
   var pass = req.body.pass
   var confPass = req.body.confPass
-  ums.register(fname, lname, email, pass, function (err, response) {
+  ums.registerUser(fname, lname, email, pass, function (err, response) {
     if (response.id != null) {
       res.render('login', {
+        loggedIn: false,
         message: "Successfully Registered, " + response.fname + ", please log in"
       });
     }
@@ -37,16 +62,5 @@ router.get('/getUserById/:id', function (req, res, next) {
   }
 });
 
-router.post('/login', function (req, res, next) {
-  var username = req.post.username;
-  var id = req.params.id;
-  var user = model.login(username, password, userOps);
-
-  function userOps(err, data) {
-    res.json({
-      'test': data
-    });
-  }
-});
 
 module.exports = router;
